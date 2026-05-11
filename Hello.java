48cf8c8ef15214b659e43c19b74138de84049340
nhatquanghoduong-svg <nhatquanghoduong@gmail.com>
2026-05-11 21:35:07 +0700

    Feature 2 - Commit 1

--- Hello.java
+++ Hello.java
@@ -1,4 +1,4 @@
 public static void main(String[] args) {
     System.out.println("Hello a new world!");
-    System.out.println("Feature 1 - Commit 2");
+    System.out.println("Feature 2 - COmmit 1");
 }
